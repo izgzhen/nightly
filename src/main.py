dff7017e10d46d56e009ae6a5f3658856839b6b3
Zhen Zhang <izgzhen@gmail.com>
2019-12-15 15:20:56 -0800

    Connect DB

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 import yaml
+import time
+import pymysql
 
-jobs_config = yaml.safe_load("config/jobs.yaml")
-resources_config = yaml.safe_load("config/resources.yaml")
+jobs_config = yaml.safe_load(open("config/jobs.yaml", "r"))
+resources_config = yaml.safe_load(open("config/resources.yaml", "r"))
 
+db = pymysql.connect(**resources_config["logdb"])
+
+while True:
+    print("Check")
+    db.ping()
+    time.sleep(1)
